f943b54578dcfc0a7bd2feefbcaeaa656f641f8b
rdario45@gmail.com <rfernandez@indigoag.com>
2021-11-23 17:14:16 -0500

    remove docker

--- bin/create_image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "docker image..."
-docker build -t beverly-api .
-
-exit 0
--- bin/run_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "running container..."
-docker run -it -p 9000:9000 --rm beverly-api
-
-exit 0
